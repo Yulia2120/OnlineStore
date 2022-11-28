76a7cd3565fa3175536173d1a726d1ae263109aa
Yuliya <iksulia1@gmail.com>
2022-11-28 09:23:45 +0200

    SeedData

--- Logic/Domain/Products/Product.cs
+++ Logic/Domain/Products/Product.cs
@@ -14,13 +14,17 @@ namespace Logic.Domain.Products
         public int Id { get; set; }
         [StringLength(200)]
         public string? Name { get; set; }
+        [StringLength(200)]
+        public string? DescrLi { get; set; }
+        [StringLength(200)]
+        public string? DescrLiSecond { get; set; }
         [StringLength(2000)]
         public string? Description { get; set; }
         [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         [StringLength(2000)]
-        public string? UrlImg { get; set; }
+        public string? ImgUrl { get; set; }
         public bool IsActive
         {
             get
--- Logic/Domain/Users/User.cs
+++ Logic/Domain/Users/User.cs
@@ -11,9 +11,14 @@ namespace Logic.Domain.Users
     public class User: IIdentity
     {
         public int Id { get; set; }
+        [Required]
         [StringLength(200)]
         public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string? Password { get; set; }
 
     }
